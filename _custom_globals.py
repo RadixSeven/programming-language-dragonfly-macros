2399fa66cc77179788e97e42e3c5e3e4f73c35bf
Eric Moyer <eric_moyer@yahoo.com>
2015-10-03 23:44:47 -0400

    Add some missing symbols

--- _custom_globals.py
+++ _custom_globals.py
@@ -1,22 +1,33 @@
-# Author:Brandon Lovrien
+# Author:Brandon Lovrien & Eric Moyer
 # This script includes some commands for various useful symbols used in programming
 
 from dragonfly import (Grammar, CompoundRule, Dictation, Text, Key, AppContext, MappingRule)
 
 # Useful commands for encapsulation of quotes, etc.
 class UsefulStuff(MappingRule):
-
+    
     mapping  = {
-                "in quotes":                Text("\"\"") + Key("left"),
-                "in single quotes":         Text("\'\'") + Key("left"),
-                "in parentheses":           Text("()") + Key("left"),
-                "in brackets":              Text("[]") + Key("left"),
-                "in braces":                Text("{}") + Key("left"),
-                "in angle brackets":        Text("<>") + Key("left"),
-                "parameters":               Text("()"),
-                "arrow":                    Text("->"),
-                "double arrow":              Text("=>"),
-               }
+        "in quotes":                Text("\"\"") + Key("left"),
+        "in single quotes":         Text("\'\'") + Key("left"),
+        "in parentheses":           Text("()") + Key("left"),
+        "in brackets":              Text("[]") + Key("left"),
+        "in braces":                Text("{}") + Key("left"),
+        "in angle brackets":        Text("<>") + Key("left"),
+        "parameters":               Text("()"),
+        "arrow":                    Text("->"),
+        "double arrow":             Text("=>"),
+        "boom":                     Text(", "),
+        "dot":                      Text("."),
+        "ace":                      Text(" "),
+        "bang":                     Text("!"),
+        "wokka":                    Text("<"),
+        "shock":                    Key("enter"),
+        "starling":                 Text("*"),
+        "grendel":                  Key("end"),
+        "carrot":                   Text("^"),
+        "hashtag":                  Text("#"),
+        
+    }
 
 globalStuff = Grammar("useful custom global commands")                # Create a grammar to contain the command rule.
 globalStuff.add_rule(UsefulStuff())
